7b35e65fd61401918be6991cdba1b8ec2e24c680
moyarka <mahyar.karimi2079@ut.ac.ir>
2022-04-24 01:19:37 +0430

    update master to read from Serial

--- master/master.ino
+++ master/master.ino
@@ -1,22 +1,32 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial hc05(2, 3); // RX | TX
+SoftwareSerial vmonitor(2, 3); // RX | TX
+
+String msg = "";
 
 void setup() {
   Serial.begin(9600);
-  hc05.begin(9600);
+  vmonitor.begin(9600);
+}
+
+char pollSerial() {
+  if (Serial.available()) {
+    char c = Serial.read(); // gets one byte from serial buffer
+    return c;
+  }
+
+  return ((char) 0);
 }
 
 void loop() {
-  while (hc05.available()) {
-    delay(10);
-    if (hc05.available() > 0) {
-      char c = hc05.read();  //gets one byte from serial buffer
-      Serial.print("Received ");
-      Serial.print(c);
-      Serial.println("");
+    char c = pollSerial();
+    
+    if (c)
+      msg += c;
+    
+    if (c == '!') {
+      vmonitor.print("MASTER: Received: ");
+      vmonitor.println(msg);
+      msg = "";
     }
-  }
-  Serial.println("No luck so far");
-  delay(200);
 }
