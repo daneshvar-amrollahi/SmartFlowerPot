675d53b0365fea01e27f614d07abe04b9f1cac5c
sinku <sininoir@gmail.com>
2022-04-28 15:59:20 +0430

    Add pwm signal generation logic

--- src/master/master.ino
+++ src/master/master.ino
@@ -8,8 +8,10 @@ struct SensorData {
 SoftwareSerial virtualMonitor(2, 3); // RX | TX
 
 String msg = "";
+int pwmOutputPin = 6;
 
 void setup() {
+  pinMode(pwmOutputPin, OUTPUT); 
   Serial.begin(9600);
   virtualMonitor.begin(9600);
 }
@@ -39,6 +41,25 @@ struct SensorData msgToSensorData(const String& msg) {
   return sensorData;
 }
 
+int convertPwmDutyCycle(int duty_cycle) {
+  int maxPwmValue = 255;
+  return (duty_cycle * maxPwmValue) / 100;
+}
+
+void generatePwmSignal(struct SensorData &sensorData) {
+  if (sensorData.humidity < 20) {
+    analogWrite(pwmOutputPin, convertPwmDutyCycle(25));
+  } else if (sensorData.humidity >= 20 && sensorData.humidity < 50) {
+     if (sensorData.cTemp > 25) {
+        analogWrite(pwmOutputPin, convertPwmDutyCycle(10));
+     } else {
+        analogWrite(pwmOutputPin, 0);
+     }
+  } else {
+    analogWrite(pwmOutputPin, 0);
+  }
+}
+
 void loop() {
     char c = pollSerial();
     
@@ -49,6 +70,7 @@ void loop() {
       virtualMonitor.print("MASTER: Received: ");
       virtualMonitor.println(msg);
       struct SensorData sensorData = msgToSensorData(msg);
+      generatePwmSignal(sensorData);
       msg = "";
     }
 }
